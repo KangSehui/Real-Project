325e7301822d80c280ba81586c8c634be628798c
my_git_test <inpyo6262@gmail.com>
2020-04-24 19:42:22 -0700

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,22 @@
 #include "calculator.h"
 
 void addition(){
-
+    cout << a + b;
+    cout << "\n";
 }
 
 void subtraction(){
-
+    cout << a - b;
+    cout << "\n";
 }
 
 void multiplication(){
+    cout << a * b;
+    cout << "\n";
 
 }
 
 void division(){
-    
+    cout << a / b;
+    cout << "\n";
 }
