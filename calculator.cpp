fbf692ac1103abaf1c33b736e8fd6f4fbe4b9012
ubuntulim <limys609@naver.com>
2020-04-25 10:59:58 +0900

    add,subt,mult,divi hw

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,26 @@
 #include "calculator.h"
+#include <stdio.h>
 
 void addition(){
+
+    printf("%.f",a+b);
 
 }
 
 void subtraction(){
 
+    printf("%.f",a-b);
+
 }
 
 void multiplication(){
 
+    printf("%.f",a*b);
+
 }
 
 void division(){
+
+    printf("%.3f",a/b);
     
 }
