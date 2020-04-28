8acf6fb3320e9dfde68fefcd5eb67f800bd37f89
kaniza96801 <21600087@handong.edu>
2020-04-28 01:50:54 -0700

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -5,13 +5,18 @@ void addition(){
 }
 
 void subtraction(){
-
 }
 
 void multiplication(){
-
+    int a1 = a, a2 = b;
+    int result = a1*a2;
+    cout<<result;
+    cout<<'\n';
 }
 
 void division(){
-    
+    int a1 = a, a2 = b;
+    int result = a1/a2;
+    cout<<a1/a2;
+    cout<<'\n';
 }
