7acdcf8a475b241c02a4a2fe075ff06ada2aedc6
ShihoonSon <sihoon197>
2020-04-28 09:31:10 -0700

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -1,12 +1,14 @@
 #include "calculator.h"
 
 void addition(){
-
+   int answer = a + b;
+    cout << "= " << answer << endl;
 }
 
 void subtraction(){
 
-
+   int answer = a - b;
+    cout << "= " << answer << endl;
 
 }
 
