03b5bd6a62966d7340093eb77a3958605c6f2f62
ShinbumKang <21500005@handong.edu>
2020-04-28 03:19:52 -0700

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -5,13 +5,15 @@ void addition(){
 }
 
 void subtraction(){
-
+    
 }
 
 void multiplication(){
-
+    double ans = a * b;
+    cout<< "= "<< ans << endl;
 }
 
 void division(){
-    
+    double ans = a / b;
+    cout<< "= "<< ans << endl;
 }
