c769014742886a35e35e64d9fc933534070bf186
Noel-git <61661237+Noel-git@users.noreply.github.com>
2020-04-29 15:59:11 +0900

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -11,11 +11,11 @@ void subtraction(){
 }
 
 void multiplication(){
-    int answer = a*b;
-    cout << "= " << answer << endl;
+    int ans = a*b;
+    cout << "= " << ans << endl;
 }
 
 void division(){
-    int answer = a/b;
-    cout << "= " << answer << endl;
+    int ans = a/b;
+    cout << "= " << ans << endl;
 }
